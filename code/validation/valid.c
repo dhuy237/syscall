ab95f6f217d8c2796736bdc834e1559d812dadc8
dhuy237 <d.huy72@gmail.com>
2019-10-04 14:56:41 +0700

    first commit

---
+++ code/validation/valid.c
@@ -0,0 +1,28 @@
+#include <get_proc_info.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[])
+{
+    int pid;
+    long sys_return_value;
+    struct procinfos info;
+    pid = atoi(argv[1]);
+
+    sys_return_value = get_proc_info(pid, &info);
+    printf("Student ID: %ld\n", info.studentID);
+    if (sys_return_value == 0)
+    {
+        printf("Current pid: %d, %s\n", info.proc.pid, info.proc.name);
+        printf("Parent pid: %d\n", info.parent_proc.pid);
+        if (info.oldest_child_proc.pid == 0)
+            printf("Dont have a child\n");
+        else
+            printf("Child pid: %d, %s\n", info.oldest_child_proc.pid, info.oldest_child_proc.name);
+    }
+    else
+        printf("Not found\n");
+    return 0;
+}
